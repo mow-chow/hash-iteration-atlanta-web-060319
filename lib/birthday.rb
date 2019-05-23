abec519e8ad18d39710618c2ecf4787f0c6fe0be
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-23 00:12:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,5 +8,7 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  else
+    "Sorry"
   end
 end
